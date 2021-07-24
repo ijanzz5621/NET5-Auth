b835dd2f5f1683badd0bda0ba92d19196df803bd
Sharizan Redzuan <sharizan@kenanga.com.my>
2021-07-24 18:03:34 +0800

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AuthAuthorize.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AuthAuthorize.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Secured()
+        {
+            return View();
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpGet("denied")]
+        public IActionResult Denied()
+        {
+            return View();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Validate(string username, string password, string returnUrl)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            if (username == "abc" && password == "123")
+            {
+                //create the claims
+                var claims = new List<Claim>();
+
+                claims.Add(new Claim(ClaimTypes.Name, "Sharizan Redzuan"));
+                claims.Add(new Claim("username", username));
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, username));
+
+                //// add claim for role
+                //claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+
+                //create claims identity
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                // create the principal
+                var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                // sign in the user
+                await HttpContext.SignInAsync(claimsPrincipal);
+
+                //redirect
+                return Redirect(returnUrl);
+            }
+
+            TempData["Error"] = "Username or Password is invalid.";
+            return View("login");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+
+            // return to homepage
+            return Redirect("/");
+        }
+
+    }
+}
